dbad703f9444f1b37536d6e02eb0bd0de755cd1b
Bernhard Esperester <bernhard@esperester.de>
2021-12-22 18:38:40 +0100

    updates setup for distribution

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="hydrogen",
     version="0.1.0",
-    packages=find_packages("src/hydrogen"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
     install_requires=[],
 )
